79d56859a1110459d0c6d3ada1c29e6009291445
Matthew A. Todd <matcatprg@yahoo.com>
2011-06-16 12:20:50 -0700

    Added licensing information

--- scripts/set_background.sh
+++ scripts/set_background.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Based on a script I found on the forums
+#
+# Matthew Todd
+
+# requires that the user change the path to a valid image on his/her system
 
 feh --bg-fill /media/Storage/Pictures/Wallpapers/fractals/NewtBasin1/c/NewtBasin1-c1.png
--- scripts/volume_status.py
+++ scripts/volume_status.py
@@ -2,8 +2,25 @@
 '''
 show the volume level in the status bar
 
+@note
 assumes pulse audio
+
+@par
 assumes only one output (sink) line
+
+@license
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 @date Jun 14, 2011
 @author Matthew Todd
